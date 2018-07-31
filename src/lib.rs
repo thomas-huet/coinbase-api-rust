9377f2eadb20ff5799617c8bcd793d07eaa3d186
Thomas HUET <git@thomash.fr>
2018-07-31 17:16:32 +0200

    Reorganize code and add doc

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,34 @@
 
 #![forbid(unsafe_code)]
 
+//! Rust client library for [Coinbase](https://docs.pro.coinbase.com).
+//!
+//! # Example
+//!
+//!```
+//!extern crate coinbase_api;
+//!extern crate hyper;
+//!
+//!use coinbase_api::*;
+//!use hyper::rt::Future;
+//!
+//!fn make_future() -> impl Future<Item=(), Error=()> {
+//!  let client = MarketDataClient::new(SANDBOX).unwrap();
+//!  client.products()
+//!  .map(|products| {
+//!    println!("Pairs available for trading:");
+//!    for p in products {
+//!      println!("{}", p.id);
+//!    }
+//!  })
+//!  .map_err(|err| println!("Error: {:?}", err))
+//!}
+//!
+//!fn main() {
+//!  hyper::rt::run(make_future());
+//!}
+//!```
+
 extern crate chrono;
 extern crate hyper;
 extern crate hyper_tls;
@@ -30,58 +58,54 @@ use hyper::{
 };
 use hyper_tls::HttpsConnector;
 
-pub const SANDBOX : &str = "https://api-public.sandbox.pro.coinbase.com";
-pub const LIVE : &str = "https://api.pro.coinbase.com";
-
-pub struct PublicClient {
-  client : Client<HttpsConnector<hyper::client::HttpConnector>, hyper::Body>,
-  base : &'static str,
-}
-
-impl PublicClient {
-  pub fn new(base : &'static str) -> Result<Self, hyper_tls::Error> {
-    let mut https = HttpsConnector::new(4)?;
-    https.https_only(true);
-    Ok(PublicClient {
-      client : Client::builder().build::<_, hyper::Body>(https),
-      base,
-    })
-  }
-
-  fn get<T>(&self, uri : String) -> impl Future<Item = T, Error = Error>
-  where
-    T : serde::de::DeserializeOwned,
-  {
-    let req = Request::builder()
-      .uri(uri)
-      .header(hyper::header::USER_AGENT, "coinbase-api-rust")
-      .body(hyper::Body::empty())
-      .unwrap();
-
-    self
-      .client
-      .request(req)
-      .and_then(|res| res.into_body().concat2())
-      .map_err(Error::HttpError)
-      .and_then(|body| {
-        serde_json::from_slice(body.as_ref())
-          .map_err(|err| Error::JsonError(err, String::from_utf8(body.as_ref().to_vec())))
-      })
-  }
-}
-
-#[derive(Debug)]
-pub enum Error {
-  HttpError(hyper::error::Error),
-  JsonError(
-    serde_json::Error,
-    Result<String, std::string::FromUtf8Error>,
-  ),
-}
-
-#[derive(Deserialize, Debug)]
+/// A decimal number with full precision.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Num(String);
 
+macro_rules! impl_num_from {
+  ($t:ty) => {
+    impl From<$t> for Num {
+      fn from(x : $t) -> Self { Num(x.to_string()) }
+    }
+  };
+}
+
+impl_num_from!(f32);
+impl_num_from!(f64);
+impl_num_from!(i8);
+impl_num_from!(i16);
+impl_num_from!(i32);
+impl_num_from!(i64);
+impl_num_from!(isize);
+impl_num_from!(u8);
+impl_num_from!(u16);
+impl_num_from!(u32);
+impl_num_from!(u64);
+impl_num_from!(usize);
+
+use std::str::FromStr;
+
+impl Num {
+  pub fn to_f32(&self) -> Option<f32> {
+    match self {
+      Num(s) => match f32::from_str(s) {
+        Ok(f) => Some(f),
+        Err(_) => None,
+      },
+    }
+  }
+
+  pub fn to_f64(&self) -> Option<f64> {
+    match self {
+      Num(s) => match f64::from_str(s) {
+        Ok(f) => Some(f),
+        Err(_) => None,
+      },
+    }
+  }
+}
+
+/// Description of a currency pair.
 #[derive(Deserialize, Debug)]
 pub struct Product {
   pub id : String,
@@ -92,25 +116,20 @@ pub struct Product {
   pub quote_increment : Num,
 }
 
-impl PublicClient {
-  pub fn products(&self) -> impl Future<Item = Vec<Product>, Error = Error> {
-    let mut uri = self.base.to_string();
-    uri.push_str("/products");
-
-    self.get(uri)
-  }
-}
-
+/// Aggregated book of orders.
 #[derive(Deserialize, Debug)]
 pub struct AggregatedBook {
   pub sequence : u64,
+  /// List of (price, size, num-orders).
   pub bids : Vec<(Num, Num, u64)>,
   pub asks : Vec<(Num, Num, u64)>,
 }
 
+/// Non aggregated book of orders.
 #[derive(Deserialize, Debug)]
 pub struct FullBook {
   pub sequence : u64,
+  /// List of (price, size, order_id).
   pub bids : Vec<(Num, Num, String)>,
   pub asks : Vec<(Num, Num, String)>,
 }
@@ -140,25 +159,7 @@ pub mod book_level {
   }
 }
 
-impl PublicClient {
-  pub fn book<T>(
-    &self,
-    id : &str,
-    level : &book_level::BookLevel<T>,
-  ) -> impl Future<Item = T, Error = Error>
-  where
-    T : serde::de::DeserializeOwned,
-  {
-    let mut uri = self.base.to_string();
-    uri.push_str("/products/");
-    uri.push_str(id);
-    uri.push_str("/book?");
-    uri.push_str(level.to_str());
-
-    self.get(uri)
-  }
-}
-
+/// Information about the last trade (tick), best bid/ask and 24h volume.
 #[derive(Deserialize, Debug)]
 pub struct Ticker {
   pub trade_id : u64,
@@ -170,13 +171,122 @@ pub struct Ticker {
   pub time : chrono::DateTime<chrono::Utc>,
 }
 
-impl PublicClient {
-  pub fn ticker(&self, id : &str) -> impl Future<Item = Ticker, Error = Error> {
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum Side {
+  Buy,
+  Sell,
+}
+
+/// Description of a trade.
+#[derive(Deserialize, Debug)]
+pub struct Trade {
+  pub time : chrono::DateTime<chrono::Utc>,
+  pub trade_id : u64,
+  pub price : Num,
+  pub size : Num,
+  /// Maker order side (buy indicates a down-tick and sell an up-tick).
+  pub side : Side,
+}
+
+/// URL for the sandbox API.
+pub const SANDBOX : &str = "https://api-public.sandbox.pro.coinbase.com";
+
+/// URL for the live API.
+/// Be sure to test your code on the sandbox API before trying the live one.
+pub const LIVE : &str = "https://api.pro.coinbase.com";
+
+/// Errors that can happen during a request to the API.
+#[derive(Debug)]
+pub enum Error {
+  HttpError(hyper::error::Error),
+  JsonError(
+    serde_json::Error,
+    Result<String, std::string::FromUtf8Error>,
+  ),
+}
+
+/// HTTP client for the unauthenticated market data API.
+pub struct MarketDataClient {
+  client : Client<HttpsConnector<hyper::client::HttpConnector>, hyper::Body>,
+  base : &'static str,
+}
+
+impl MarketDataClient {
+  /// Creates a new client.
+  /// The `base` argument should be `SANDBOX` or `LIVE`.
+  pub fn new(base : &'static str) -> Result<Self, hyper_tls::Error> {
+    let mut https = HttpsConnector::new(4)?;
+    https.https_only(true);
+    Ok(MarketDataClient {
+      client : Client::builder().build::<_, hyper::Body>(https),
+      base,
+    })
+  }
+
+  fn get<T>(&self, uri : String) -> impl Future<Item = T, Error = Error>
+  where
+    T : serde::de::DeserializeOwned,
+  {
+    let req = Request::builder()
+      .uri(uri)
+      .header(hyper::header::USER_AGENT, "coinbase-api-rust")
+      .body(hyper::Body::empty())
+      .unwrap();
+    self
+      .client
+      .request(req)
+      .and_then(|res| res.into_body().concat2())
+      .map_err(Error::HttpError)
+      .and_then(|body| {
+        serde_json::from_slice(body.as_ref())
+          .map_err(|err| Error::JsonError(err, String::from_utf8(body.as_ref().to_vec())))
+      })
+  }
+
+  /// Retrieves a list of available currency pairs for trading.
+  pub fn products(&self) -> impl Future<Item = Vec<Product>, Error = Error> {
+    let mut uri = self.base.to_string();
+    uri.push_str("/products");
+    self.get(uri)
+  }
+
+  /// Retrieves a list of open orders for a product.
+  /// The amount of detail shown depends on the level argument:
+  /// - `book_level::Best()` shows only the best bid and ask.
+  /// - `book_level::Top50()` shows the top 50 aggregated bids and asks.
+  /// - `book_level::Full()` shows the full non aggregated order book.
+  pub fn book<T>(
+    &self,
+    product_id : &str,
+    level : &book_level::BookLevel<T>,
+  ) -> impl Future<Item = T, Error = Error>
+  where
+    T : serde::de::DeserializeOwned,
+  {
     let mut uri = self.base.to_string();
     uri.push_str("/products/");
-    uri.push_str(id);
-    uri.push_str("/ticker");
+    uri.push_str(product_id);
+    uri.push_str("/book?");
+    uri.push_str(level.to_str());
+    self.get(uri)
+  }
 
+  /// Retrieves information about the last trade (tick), best bid/ask and 24h volume.
+  pub fn ticker(&self, product_id : &str) -> impl Future<Item = Ticker, Error = Error> {
+    let mut uri = self.base.to_string();
+    uri.push_str("/products/");
+    uri.push_str(product_id);
+    uri.push_str("/ticker");
+    self.get(uri)
+  }
+
+  /// Lists the latest trades for a product.
+  pub fn trades(&self, product_id : &str) -> impl Future<Item = Vec<Trade>, Error = Error> {
+    let mut uri = self.base.to_string();
+    uri.push_str("/products/");
+    uri.push_str(product_id);
+    uri.push_str("/trades");
     self.get(uri)
   }
 }
